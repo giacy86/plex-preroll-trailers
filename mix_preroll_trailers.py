a23415293977b35a138e714797f0ad156f73f79a
giacy86 <giaci86@hotmail.it>
2018-04-29 08:21:43 +0200

    Update mix_preroll_trailers.py
    Added a variable in the settings to allow random rotation for more than 10 videos.

--- mix_preroll_trailers.py
+++ mix_preroll_trailers.py
@@ -6,14 +6,15 @@ import os
 #	Settings
 ###################################################################################
 
-quantity = 3		# number of trailers to concatenate. Only these trailers will be played
+quantity = 3			# number of trailers to concatenate. Only these trailers will be played
+max_trailers = 10		# max number of trailers downloadED. 
 
 ###################################################################################
 
 trailers = json.load(open('preroll_trailers.json'))
 print json.dumps(trailers, ensure_ascii=False, sort_keys=True, indent=4)
 
-selected_trailers = random.sample(xrange(1,11),quantity)
+selected_trailers = random.sample(xrange(1,max_trailers+1),quantity)
 	
 print selected_trailers
 
