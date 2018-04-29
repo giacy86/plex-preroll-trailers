3fd09283a484d6692804fea10d13dd91df13c3a0
giacy86 <giaci86@hotmail.it>
2018-04-29 08:19:14 +0200

    Update download_preroll_trailers.py
    Modified for a more accurate search of localized trailers. If TheMovieDB not return a localized trailer, instead of download the english version go to youtube and search for the trailer with a query like "<movie title> trailer <language>".

--- download_preroll_trailers.py
+++ download_preroll_trailers.py
@@ -10,9 +10,12 @@ import os
 ###################################################################################
 
 tmdb.API_KEY = "635fa65913bd0eca7a06e6722ca71e1c"
-language = "it-IT"
-region = "it"
-max_trailers = 10						# max number of trailers to download
+language = "it-IT"						# Pass a ISO 639-1 value to display translated data for the fields that support it.
+region = "IT"							# Specify a ISO 3166-1 code to filter release dates. Must be uppercase.
+max_trailers = 10						# max number of trailers to download. 
+										# NOTE: change this setting to mix_preroll_trailers.py
+query_region = "ita"					# a string to append at the query for searching localized trailer. ie: 
+							# "<movie name> + trailer + <ita>"
 
 ###################################################################################
 
@@ -25,6 +28,7 @@ def getKey(item):
 
 movies = tmdb.Movies()
 
+# Get movies NOW PLAYING
 movies.now_playing(page=1, language=language, region=region)
 
 i = 0
@@ -33,20 +37,20 @@ for s in movies.results:
 		break
 	print(s['title'], s['id'], s['release_date'], s['popularity'])
 	x = tmdb.Movies(s['id'])
-	try:
+	try:	# if there is a localized trailers into ThMovieDB response
 		trailer = x.videos(language='it-IT')
-		#print trailer['results'][0]['key']
-		youtube_keys.append([trailer['results'][0]['key'],s['popularity']])
+		youtube_keys.append(['https://www.youtube.com/watch?v='+trailer['results'][0]['key'],s['popularity']])
+		#print("youtube_keys: ")	# for debug
+		#print(youtube_keys)		# for debug
 	except IndexError:
 		try:
-			trailer = x.videos()
-			#print trailer['results'][0]['key']
-			youtube_keys.append([trailer['results'][0]['key'],s['popularity']])
+			youtube_keys.append([s['title']+" trailer "+query_region,s['popularity']])
+			print("Localized trailer not found. Searching for \""+s['title']+" trailer "+query_region+"\"")
 		except IndexError:
 			break
 	i = i+1
 	
-
+# Get movies UPCOMING
 movies.upcoming(language=language, region=region)
 
 j = 0
@@ -55,47 +59,41 @@ for s in movies.results:
 		break
 	print(s['title'], s['id'], s['release_date'], s['popularity'])
 	x = tmdb.Movies(s['id'])
-	try:
+	try:	# if there is a localized trailers into ThMovieDB response
 		trailer = x.videos(language='it-IT')
-		#print trailer['results'][0]['key']
-		youtube_keys.append([trailer['results'][0]['key'],s['popularity']])
+		youtube_keys.append(['https://www.youtube.com/watch?v='+trailer['results'][0]['key'],s['popularity']])
+		#print("youtube_keys: ")	# for debug
+		#print(youtube_keys)		# for debug
 	except IndexError:
 		try:
-			trailer = x.videos()
-			#print trailer['results'][0]['key']
-			youtube_keys.append([trailer['results'][0]['key'],s['popularity']])
+			youtube_keys.append([s['title']+" trailer "+query_region,s['popularity']])
+			print("Localized trailer not found. Searching for \""+s['title']+" trailer "+query_region+"\"")
 		except IndexError:
 			break
 	j = j+1
 
 trailers = sorted(youtube_keys, key=getKey, reverse=True)		
 
-#trailers_filename = []	#array with the filename of the trailers
+
 trailers_filename = {}	#list with the filename of the trailers
 k = 0
 
 for k in range(0,max_trailers):
+	filename = 'trailer_'+str(k)+'.mp4'
 	ydl_opts = {
-		'outtmpl': '%(id)s.%(ext)s',
+		'outtmpl': filename,
 		'format': 'best[ext=mp4]',
+		'default_search': 'ytsearch1:',
+		'restrict-filenames': 'TRUE',
 		}
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		#ydl.download(['https://www.youtube.com/watch?v='+trailers[k][0]])
-		video=ydl.extract_info('https://www.youtube.com/watch?v='+trailers[k][0], download=True)
-		filename=ydl.prepare_filename(video)
-		#trailers_filename.append(filename.encode('utf8'))
+		video=ydl.extract_info(trailers[k][0], download=True)
+		#filename=ydl.prepare_filename(video)
 		trailers_filename[k+1] = filename
-		#os.symlink(filename, 'trailer_'+str(k))
-
-# Write the filename of the trailers into a file
-# with open("preroll_trailers.txt", 'w') as f:
-	# for t in trailers_filename:
-		# f.write(t + os.linesep)
-# f.close	
+	
 
 # Write the filename of the trailers into a JSON file
 print json.dumps(trailers_filename, ensure_ascii=False, sort_keys=True, indent=4)
 with open("preroll_trailers.json", 'w') as f:
 	json.dump(trailers_filename, f)
 f.close	
-	#print trailers[k][0]
